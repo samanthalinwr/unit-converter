8f73fe836347794785955dbfe4b9983bfbe516b0
Samantha Lin <samanthalin.wr@gmail.com>
2025-08-12 23:42:50 +0000

    small change

--- unit-converter/next.config.mjs
+++ unit-converter/next.config.mjs
@@ -3,9 +3,9 @@ const isProd = process.env.GITHUB_PAGES === 'true';
 const repo = 'unit-converter'; // <-- your repo name
 
 export default {
-  output: 'export',              // replaces `next export`
-  images: { unoptimized: true }, // required for GitHub Pages
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
-  trailingSlash: true
+    output: 'export',              // replaces `next export`
+    images: { unoptimized: true }, // required for GitHub Pages
+    basePath: '/unit-converter',
+    assetPrefix: '/unit-converter/',
+    trailingSlash: true
 };
